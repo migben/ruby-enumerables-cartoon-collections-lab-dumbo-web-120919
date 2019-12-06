c77d1d5a17adbbae51264cfaa2bde039ab93d74a
Miguel Ben <itsmigben@gmail.com>
2019-12-06 21:43:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+# code goes below this line
+def roll_call_dwarves(dwarf)
   # Your code here
+  
+  dwarf.each_with_index {|d, idx|
+    puts "#{idx + 1}. #{d}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(caps)
   # Your code here
+  
+  caps.map{|calling| calling.capitalize + "!"}
+  
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(caps)
   # Your code here
+  caps.any? {|calling| calling.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(precious_cheese)
   # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
